2ba5d2c4e63b05b9a7dcc48dac97be051c84c5ba
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-10-19 06:10:37 +0700

    feat: writing your first django app, part 2

---
+++ Makefile
@@ -0,0 +1,6 @@
+runserver:
+	python3 manage.py runserver
+migrate:
+	python3 manage.py migrate
+shell:
+	python3 manage.py shell
--- getting_started/settings.py
+++ getting_started/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--u%p2c%)9kspbrzkxl9#=^f38_v-!euh&*5gg6ho^b+!_1+^_&'
+SECRET_KEY = 'django-insecure-43n$eew6in-ob_n)b1qs7*6ezrsq0hjgv^+(!hk_2&da(m*==q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
